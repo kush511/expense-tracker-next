d638699ef43b2524b0f5daddfb0e660e76095fe0
Kush Kathuria <kushkathuria511@gmail.com>
2025-07-29 19:35:03 -0700

    Contact and about page added

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,5 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 
 export default function Page() {
-  return <SignIn />
+  return <div className="my-6 flex items-center justify-center">
+    <SignIn />
+  </div>
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 
 export default function Page() {
-  return <SignUp />
+  return <div className="my-6 flex items-center justify-center">
+      <SignUp />
+    </div>
+  
 }
